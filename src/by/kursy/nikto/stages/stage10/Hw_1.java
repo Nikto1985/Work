98dc250be614473396bf8ed872324fcb76c6161d
Nikto1985 <logical.controllers@gmail.com>
2022-04-04 00:11:39 +0300

    hhh

---
+++ src/by/kursy/nikto/stages/stage10/Hw_1.java
@@ -0,0 +1,17 @@
+package by.kursy.nikto.stages.stage10;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Hw_1 {
+    public static int dice(){
+        Random random = new Random();
+        int num1 = random.nextInt(6);
+        int num2 = random.nextInt(6);
+        int sum = num1 + num2 + 2;
+        return sum;
+    }
+    public static void main(String[] args){
+        System.out.println(dice());
+    }
+}
