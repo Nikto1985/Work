4b300dfdcf15977c694ccf7d80471a5c949fec9f
Nikto1985 <logical.controllers@gmail.com>
2022-04-16 19:24:07 +0300

    Inserted Sort

--- src/by/kursy/buyel/javalessons/lesson14/ArraySorter.java
+++ src/by/kursy/buyel/javalessons/lesson14/ArraySorter.java
@@ -33,4 +33,33 @@ public class ArraySorter {
             }
         }
     }
+    public static void insertedSortAcs(int[] array){
+        for (int i = 1; i < array.length; i++) {
+            int buf = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > buf) {
+                array[j + 1] = array[j];
+                j--;
+
+            }
+
+            array[j + 1] = buf;
+        }
+    }
+    public static void selectedSortAcs(int[] array){
+        for (int i = 0; i < array.length - 1; i++){
+            int minim = array[i];
+            int index = i;
+            for (int j = i; j < array.length; j++){
+                if (array[j] < minim){
+                    minim = array[j];
+                    index = j;
+                }
+            }
+            array[index] = array[i];
+            array[i] = minim;
+
+        }
+    }
 }
--- src/by/kursy/buyel/javalessons/lesson14/task02/Main.java
+++ src/by/kursy/buyel/javalessons/lesson14/task02/Main.java
@@ -1,5 +1,6 @@
 package by.kursy.buyel.javalessons.lesson14.task02;
 
+import by.kursy.buyel.javalessons.lesson14.ArraySorter;
 import by.kursy.buyel.javalessons.view.Printer;
 
 import java.util.Arrays;
@@ -8,10 +9,12 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {1, 5 , 8 , 9 , 2, 3, 7, 4};
 
-        Printer.print("Before sorting: ");
+        Printer.print("Before sorting: \n");
         Printer.print(Arrays.toString(array));
 
-        Printer.print("After sorting: ");
+        ArraySorter.insertedSortAcs(array);
+
+        Printer.print("\nAfter sorting: \n");
         Printer.print(Arrays.toString(array));
     }
 }
