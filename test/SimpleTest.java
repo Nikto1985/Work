31392017da71ff00845de1d23f4aeb4c137fca02
Nikto1985 <logical.controllers@gmail.com>
2022-04-16 20:21:09 +0300

    Test

---
+++ test/SimpleTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+public class SimpleTest {
+    @Test
+    public void test(){
+        // arrange
+        int a = 10;
+        int b = 20;
+        int expected = 30;
+
+        // action
+        int actual = a + b;
+        // assert
+        if (expected != actual){
+            Assert.fail();
+        }
+    }
+}
