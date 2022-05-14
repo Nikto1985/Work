de294da125b9408feb038f41d3dcc696bae72299
Nikto1985 <logical.controllers@gmail.com>
2022-05-14 16:43:54 +0300

    Using private modifier

--- src/by/kursy/buyel/javalessons/lesson17/model/Student.java
+++ src/by/kursy/buyel/javalessons/lesson17/model/Student.java
@@ -1,7 +1,20 @@
 package by.kursy.buyel.javalessons.lesson17.model;
 
 public class Student {
-    public String name;
+    private String name;
+
+    public Student(){
+        name =  "no name";
+    }
+
+    public Student(String name){
+        this.name = name;
+    }
+
+    public Student(Student student){
+        //this.name = student.name;
+        this(student.name);
+    }
 
     public void change(){
         name = "Alex";
